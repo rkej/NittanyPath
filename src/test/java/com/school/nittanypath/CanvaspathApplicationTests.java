11f769b47e39b809b4d829bc3e44362dd2e8cf8d
rkej <rahulkejriwal18@gmail.com>
2020-03-18 02:25:21 -0400

    First commit - Spring Initializr, React setup, data import and password encrypted

---
+++ src/test/java/com/school/nittanypath/CanvaspathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.nittanypath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvaspathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
